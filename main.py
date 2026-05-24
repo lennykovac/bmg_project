7c000abac74788c00126a4da9438dd7029256f20
= <lennykovac@pm.me>
2026-05-24 09:23:28 +0200

    fooling around

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import networkx as nx
 from asymmetree.analysis.best_matches import bmg_from_tree
 from asymmetree.utils.phylogenetic_trees import random_colored_tree
 
+"""
+Convert tralda.Tree into network x
+"""
+
 
 def main():
     n = 2
@@ -18,4 +22,15 @@ def main():
 GOAL: Edit a bicolored tree into a phylogenetic network by inserting hybridization vertices
 """
 if __name__ == "__main__":
-    main()
+    # main()
+    n = 15
+    colors = 5
+    bic_tree = random_colored_tree(n, colors)
+
+    graph, root_id = bic_tree.to_nx()
+
+    nx.write_gml(graph, "yikes.gml")
+    """
+    for node, data in graph.nodes.data:
+        print(node)
+    """
