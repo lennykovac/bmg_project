f5f5a6457079423ce8d8b7635aacb2e66c360d7d
= <lennykovac@pm.me>
2026-06-01 15:02:59 +0200

    example in main

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-import networkx as nx
-
 from utils.graph_utils import show_graph, transform
+from utils.tree_utils import create_gene_tree
 
 if __name__ == "__main__":
-    G = nx.read_gml("tests/gene_tree_test_file.gml")
+    # G = nx.read_gml("tests/gene_tree_test_file.gml")
+    species = 10
+    species_tree_age = 1
+    trees = create_gene_tree(species, species_tree_age)
 
-    G_transformed = transform(G, 10)
+    gene_tree_di_graph = trees.gene_tree
+    species_tree_di_graph = trees.gene_tree
+
+    G_transformed = transform(gene_tree_di_graph, 10)
     show_graph(G_transformed)
