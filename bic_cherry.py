e0930662adfa83d81fa9635ba28748bf2c9a93f5
tucama <xxtucamarxx@gmail.com>
2026-06-02 10:58:27 +0200

    adapt bic cherry for BMGs

--- bic_cherry.py
+++ bic_cherry.py
@@ -5,27 +5,23 @@ import networkx as nx
 
 AVAILABLE_COLORS = ['tab:red', 'tab:blue', 'tab:green', 'tab:yellow', 'tab:cyan', 'tab:purple', 'tab:gray', 'tab:orange']
 
-def get_species(node) -> tuple[str, int]:
-    node_attributes = dict(node.attributes())
-    #(label, species)
-    return (node_attributes['label'], node_attributes['reconc'])
-
-def build_species_color_map(tree, colors: list[str]) -> dict:
+def build_species_color_map(bmg, colors: list[str]) -> dict:
     species_color_map = dict()
-    for node in tree.leaves():
-        _, node_species = get_species(node)
+    for node, data in bmg.nodes(data=True):
+        node_species = data.get("color", "None")
 
         if node_species not in species_color_map.keys():
             species_color_map[node_species] = colors.pop(0)
+
     return species_color_map
 
-def init_graph(tree, colors) -> nx.DiGraph:
+def init_graph(bmg, colors) -> nx.DiGraph:
     graph = nx.DiGraph(name="bic_cherry")
     graph.add_node("r", color="black", kind='root', layer=3, label="Root")
 
-    for node in tree.leaves():
-        node_label, node_species = get_species(node)
-        graph.add_node(node_label, color=colors[node_species], kind='leaf', layer=0, label=node_label)
+    for node, data in bmg.nodes(data=True):
+        node_species = data.get("color", "None")
+        graph.add_node(node, color=colors[node_species], kind='leaf', layer=0, label=node)
     return graph
 
 def add_cherry_parents(graph: nx.DiGraph) -> nx.DiGraph:
@@ -49,6 +45,9 @@ def add_cherry_parents(graph: nx.DiGraph) -> nx.DiGraph:
     return graph
 
 def bic_extension(graph: nx.DiGraph, parent_node, child_node, extension_leaf_node) -> nx.DiGraph:
+    if (extension_leaf_node, child_node) not in graph.edges:
+        raise ValueError("extension edge not in bmg")
+
     if (parent_node, extension_leaf_node) not in graph.edges:
         extension_node = f"q_{child_node}-{extension_leaf_node}"
         extension_label = f"Q_{child_node}-{extension_leaf_node}"
@@ -65,25 +64,45 @@ def bic_extension(graph: nx.DiGraph, parent_node, child_node, extension_leaf_nod
 def draw_graph(graph: nx.DiGraph) -> None:
     node_colors = list()
     node_sizes = list()
+    node_labels = list()
+    edge_colors = list()
 
-    for _, data in graph.nodes(data=True):
+    for name, data in graph.nodes(data=True):
         node_sizes.append(data.get('size', 10000))
         node_colors.append(data.get('color', 'lightgray'))
+        node_labels.append(data.get('label', name))
+
+    for origin, target, data in graph.edges(Data=True):
+        edge_colors.append(data.get('edge_color', 'black'))
 
     pos = nx.multipartite_layout(graph, subset_key='layer', align='horizontal')
 
-    nx.draw(graph,
-            pos=pos,
-            with_labels=True,
-            node_color=node_colors,
-            # arrows=True,
-            node_size=node_sizes,
-            font_weight=1000,
-            font_size=20,
-            font_color="White",
-            linewidths=3.0,
-            edgecolors="black",
-            arrowsize=50)
+    nx.draw_networkx_nodes(
+        graph,
+        pos=pos,
+        node_color=node_colors,
+        node_size=node_sizes,
+        linewidths=3.0,
+        edgecolors="black",
+    )
+
+    nx.draw_networkx_edges(
+        graph,
+        pos=pos,
+        edge_color=edge_colors,
+        arrows_size=20,
+        width=5,
+        node_size=node_sizes,
+    )
+
+    nx.draw_networkx_labels(
+        graph,
+        pos=pos,
+        font_weight=1000,
+        font_size=20,
+        font_color="White",
+    )
+
     plt.show()
     return
 
