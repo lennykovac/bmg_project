fc2eb77715d80419c372958f2ba97d15934e400a
Leo <carlcarios31@gmail.com>
2026-08-02 18:51:51 +0200

    moved test

--- obj_1a.py
+++ obj_1a.py
@@ -2,31 +2,17 @@ from utils.graph_utils import show_graph, transform, bmg_from_network, wbmg_from
 from utils.tree_utils import create_gene_tree
 import networkx as nx
 
-# False basic example
-G = nx.DiGraph()
-
-G.add_node(0, label="0", reconc="0")
-G.add_node(1, label="1", reconc="0")
-G.add_node(2, label="2", reconc="0")
-G.add_node(3, label="3", reconc="0")
-G.add_node(4, label="4", reconc="0")
-G.add_node(5, label="5", reconc="1")
-G.add_node(6, label="6", reconc="0")
-
-G.add_edges_from([(0, 1), (0, 2), (1, 3), (2, 4), (1, 4), (3, 5), (3, 6), (2, 5)])
-
-# True basic example
-
-Gt = nx.DiGraph()
-
-Gt.add_node(0, label="0", reconc="0")
-Gt.add_node(1, label="1", reconc="0")
-Gt.add_node(2, label="2", reconc="1")
-
-Gt.add_edges_from([(0, 1), (0, 2)])
-
 
 def bmg_wbmg_check(G: nx.DiGraph):
+    """
+    Checks whether best matches are also weak best matches and vice versa.
+
+    Parameters:
+    G: Directed Graph
+
+    Returns:
+    Boolean values bmg_implies_wbmg and wbmg_implies_bmg.
+    """
 
     bmg = bmg_from_network(G)
     wbmg = wbmg_from_network(G)
@@ -58,28 +44,28 @@ def bmg_wbmg_check(G: nx.DiGraph):
     return bmg_implies_wbmg, wbmg_implies_bmg
 
 
-# basic test
-# also_wbmg, also_bmg = bmg_wbmg_check(Gt)
-# print(also_bmg, also_wbmg)
+def main():
+    species = 10
+    species_tree_age = 1
+    # we know bmg -> wbmg. But not wbmg->bmg. So if also_wbmg is false, our theoretical prediction would be wrong
+    expected_combination = 0
+    wrong_combinations = 0
+    for i in range(1000):
+        trees = create_gene_tree(species, species_tree_age)
 
-species = 10
-species_tree_age = 1
-# we know bmg -> wbmg. But not wbmg->bmg. So if also_wbmg is false, our theoretical prediction would be wrong
-expected_combination = 0
-wrong_combinations = 0
-for i in range(1000):
-    trees = create_gene_tree(species, species_tree_age)
+        gene_tree_di_graph = trees.gene_tree
 
-    gene_tree_di_graph = trees.gene_tree
-    species_tree_di_graph = trees.gene_tree
+        G_transformed = transform(gene_tree_di_graph, 10)
+        also_bmg, also_wbmg = bmg_wbmg_check(G_transformed)
+        if also_wbmg:
+            expected_combination += 1
+        else:
+            wrong_combinations += 1
 
-    G_transformed = transform(gene_tree_di_graph, 10)
-    also_bmg, also_wbmg = bmg_wbmg_check(G_transformed)
-    if also_wbmg:
-        expected_combination += 1
-    else:
-        wrong_combinations += 1
+    print(
+        expected_combination, wrong_combinations
+    )  # checked with 1000 networks and 10 species each, all as expected
 
-print(
-    expected_combination, wrong_combinations
-)  # checked with 1000 networks and 10 species each, all as expected
+
+if __name__ == "__main__":
+    main()
